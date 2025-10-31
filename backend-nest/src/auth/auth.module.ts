66e83787f9cad8c12be268b85049892274d7abe8
Gokhanagingil <gokhan.agingil@gmail.com>
2025-10-31 20:15:39 +0300

    fix(backend): add TenantEntity to AuthModule for TypeORM relations

--- backend-nest/src/auth/auth.module.ts
+++ backend-nest/src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserEntity } from '../entities/auth/user.entity';
+import { TenantEntity } from '../entities/tenant/tenant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, TenantEntity])],
   controllers: [AuthController],
   providers: [AuthService],
 })
